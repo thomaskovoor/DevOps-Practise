deca12500fca4847bcc5e7713c8081def952cdbe
thomaskovoor <thomaskovoor123@gmail.com>
2024-02-16 14:26:10 +0530

    first commit

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>This a test web application for Devops Training</h1>"
+
+@app.route('/user/<name>')
+def user(name):
+    return f"<h1>Hello, {name}!</h1>"
+
+if __name__ == '__main__':
+    app.run(host ='0.0.0.0', port = 5001, debug = True)
