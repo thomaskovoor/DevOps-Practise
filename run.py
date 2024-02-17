08b4f361834880712eaa806f4bf31c3f7a8d882b
thomaskovoor <thomaskovoor123@gmail.com>
2024-02-17 20:42:18 +0530

    dummy commit-6

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>This a test web application for Devops Training,lessgooo devops</h1>"
+    return "<h1>This a test web application for Devops Training,lets go devops</h1>"
 
 @app.route('/user/<name>')
 def user(name):
