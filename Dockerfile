1087a4d1dafa0c9dfcb86823e5e06fa13e846bb4
thomaskovoor <thomaskovoor123@gmail.com>
2024-02-17 21:28:35 +0530

    dummy commit-7

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:3-alpine3.12
 WORKDIR /app
 COPY . /app
 RUN pip install -r requirements.txt
-EXPOSE 8080
+EXPOSE 5000
 CMD ["python", "run.py"]
--- run.py
+++ run.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>This a test web application for Devops Training,lets go devops</h1>"
+    return "<h1>This a test web application for Devops Training,go devops</h1>"
 
 @app.route('/user/<name>')
 def user(name):
     return f"<h1>Hello, {name}!</h1>"
 
 if __name__ == '__main__':
-    app.run(host ='0.0.0.0', port = 8080, debug = True)
+    app.run(host ='0.0.0.0', port = 5000, debug = True)
